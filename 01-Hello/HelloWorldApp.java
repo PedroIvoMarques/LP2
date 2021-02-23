b407802a9771f2084b8c7fad26f1ffe44889c2c5
Pedro Ivo <43143773+PedroIvoMarques@users.noreply.github.com>
2021-02-23 19:09:09 -0300

    Add files via upload
    EX. 1.2 ver.1

---
+++ 01-Hello/HelloWorldApp.java
@@ -0,0 +1,10 @@
+/**
+ * Ex 1 de LP2
+ * Uma classe que "printa" a frase "Hello World!"
+ */
+
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
